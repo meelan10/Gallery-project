0769315c73f16b0987bbcb4a56fc1e7453e49c2c
meelan10 <milanwagle73@gmail.com>
2026-05-28 21:37:38 +0545

    using axios method

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const App = () => {
+
+const getData = () => {
+ 
+}
+
   return (
     <div className='bg-black h-screen text-white'>
-      <button className='bg-green-600 px-5 text-white rounded py-2 m-5'>get data</button>
+      <button 
+      onClick={getData}
+      className='bg-green-600 px-5 text-white rounded py-2 m-5 active:scale-95'
+      >
+        get data
+      </button>
     </div>
   )
 }
