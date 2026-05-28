9c2282fdca2e230cf4d55c5ab966d0fd028c10fe
meelan10 <milanwagle73@gmail.com>
2026-05-28 21:30:17 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black h-screen text-white'>
+      <button className='bg-green-600 px-5 text-white rounded py-2 m-5'>get data</button>
+    </div>
+  )
+}
+
+export default App
