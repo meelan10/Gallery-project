d0bffd946a4bab45627027ebea022a40e47b3dba
meelan10 <milanwagle73@gmail.com>
2026-05-31 14:00:13 +0545

    pegination added

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,59 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
   const [userData, setuserData] = useState([]);
+  const [index, setindex] = useState(1)
 
 const getData =async () => {
   const response =await axios.get('https://picsum.photos/v2/list?page=3&limit=30')
 
-  setuserData(response.data)
-
-  console.log(response.data);
+  setuserData(response.data);
 }
 
-   let printUserData = 'No User Available'
+useEffect(function(){
+  getData()
+},[])
+  
+   let printUserData = <h3 className='text-gray-400'>No user Available</h3>
    
    if (userData.length>0){
     printUserData = userData.map(function(elem , idx){
-      return <div>
-        <div className='h-40 w-44 bg-white rounded-xl'>
-        <img className='h-[90%] object-cover ' src={elem.download_url} alt='' />
+      return <div key={idx}>
+       <a href={elem.url} target='_blank'>
+         <div className='h-40 w-44 bg-white rounded-xl overflow-hidden'>
+        <img className='h-full object-cover w-full' src={elem.download_url} alt='' />
       </div>
-      <h2>{elem.author}</h2>
+      <h2 className='font-bold'>{elem.author}</h2>
+      </a>
       </div>
     })
    }
 
   return (
     <div className='bg-black overflow-auto h-screen text-white'>
-      <button 
-      onClick={getData}
-      className='bg-green-600 px-5 text-white rounded py-2 m-5 active:scale-95'
-      >
-        Get data
-      </button>
-      <div className='flex flex-wrap gap-4' >
+      <h2 className='text-6xl fixed bg-red-500'>{index}</h2>
+      <div className='flex flex-wrap gap-4 p-2' >
         {printUserData}
+      </div>
+      <div className='flex justify-center items-center p-4'>
+        <button className='bg-amber-400 text-sm text-black rounded px-4 py-2 font-semibold cursor-pointer active:scale-95'
+        onClick={()=>{
+        if(index>0) {
+          setindex(index-1)
+        }
+        }}
+        >prev...
+        
+        </button>
+        <button 
+        className='bg-amber-400 text-sm text-black rounded px-4 py-2 font-semibold cursor-pointer active:scale-95'
+        onClick={()=>{
+        setindex(index + 1)
+        }}
+        >
+          Next
+        </button>
       </div>
     </div>
   )
